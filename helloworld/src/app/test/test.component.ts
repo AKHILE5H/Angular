ced51c1de8d30fb91695200e595ac7179823d2b2
AKHILE5H <akhileshingole07@gmail.com>
2024-01-21 19:55:44 +0530

    Interpolation done

--- helloworld/src/app/test/test.component.ts
+++ helloworld/src/app/test/test.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrl: './test.component.css'
+  template: `
+    <h1>Hello {{ name }}</h1>
+    <h1>{{"Welcome "+name}}</h1>
+    <h1>{{2+2}}</h1>
+    <h1>Length: {{name.length}}</h1>
+    <h1>{{greetuser()}}</h1>
+    <h1>{{siteurl}}</h1>
+
+      <!-- gives errors
+    <h1>{{a=2+2}}</h1>
+    <h1>{{window.location.href}}</h1> -->
+  `,
+  styles: ['p { color: red; }']
 })
 export class TestComponent {
+  public name = "Akhilesh";
+  siteurl = window.location.href;
 
+  greetuser(){
+    return "Frome greet function Hello "+ this.name
+  }
 }
