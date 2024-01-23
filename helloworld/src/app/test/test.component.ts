dd4e0e140a151a488f0608b2dbecb66324d80197
AKHILE5H <akhileshingole07@gmail.com>
2024-01-23 13:11:17 +0530

    Property binding done

--- helloworld/src/app/test/test.component.ts
+++ helloworld/src/app/test/test.component.ts
@@ -13,12 +13,23 @@ import { Component } from '@angular/core';
       <!-- gives errors
     <h1>{{a=2+2}}</h1>
     <h1>{{window.location.href}}</h1> -->
+
+    <!-- PROPERTY BINDING -->
+    <input type="text" [id]="myid" [disabled]="boolvalue" value="Akhilesh">
+    <input type="text" id={{myid}} bind-disabled="boolvalue" value="Akhilesh">
+    
+
+    <!-- interpolation only works with string value so we use property binding by using [] -->
+
   `,
   styles: ['p { color: red; }']
 })
 export class TestComponent {
   public name = "Akhilesh";
+  public myid = "testid";
+  public boolvalue = false;
   siteurl = window.location.href;
+
 
   greetuser(){
     return "Frome greet function Hello "+ this.name
